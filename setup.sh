c5c6816d9422a6f8d0b98ef29ca2f187f4f43e86
Joseph Robinson <smartech56@gmail.com>
2017-11-07 13:47:08 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,9 +8,14 @@ echo installing dependencies
 sudo apt update
 sudo apt install toilet
 # Gideon depends
-echo installing dependencies ...
-sudo apt install wget git curl unzip python-pip python-dev build-essential 
+echo installing dependencies (python)...
+sudo apt install wget git curl unzip python-pip python-dev build-essential -y
 sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
+echo installing dependencies (webserver)...
+apt install apache2 apache2-doc apache2-utils -y
+cd /var/www
+wget https://raw.githubusercontent.com/josephworks/Gideon/master/gideon.html
+servise appache restart
 pip3 install pyvmomi
 #-----------------------------------------------
